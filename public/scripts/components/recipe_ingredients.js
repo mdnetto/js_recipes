accc441cffadba2404ba65604b6477a5c4034adc
Michelle D'Netto <mdnetto@gmail.com>
2016-09-08 17:16:38 -0400

    Trying to fix indentation

--- public/scripts/components/recipe_ingredients.js
+++ public/scripts/components/recipe_ingredients.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class RecipeIngredients extends Component {
 	constructor() {
-	    super();
-			this.intialiseIngredientOnEnter = this.initialiseIngredientOnEnter.bind(this);
+	  super();
+		this.intialiseIngredientOnEnter = this.initialiseIngredientOnEnter.bind(this);
 	}
 
   initialiseIngredientOnEnter(e) {
@@ -18,18 +18,13 @@ class RecipeIngredients extends Component {
     return (
       <div className='recipeIngredients'>
         <h1>Ingredients</h1>
-			{this.props.ingredients.map(function(ingredient, i) {
-			    return(
-					<p key={i}>
-						<input autoFocus
-						type='text'
-						value={ingredient.name}
-					    onChange={function(e) {that.props.handleIngredientNameEdit(e.target.value, i)}}
-					    onKeyDown={that.initialiseIngredientOnEnter}
-						/>
-					</p>
-			   )})
-		    }
+					{this.props.ingredients.map(function(ingredient, i) {
+						return(
+							<p key={i}>
+								<input autoFocus type='text' value={ingredient.name} onChange={function(e) {that.props.handleIngredientNameEdit(e.target.value, i)}} onKeyDown={that.initialiseIngredientOnEnter} /> 
+							</p>
+						)})
+					}
       </div>
     );
   }
